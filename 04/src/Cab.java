90141c601ec75ba59ec47562fc3f5e9ebb3cc26c
Peter Borovansky <peter.borovansky@microstep-mis.com>
2021-03-10 08:37:07 +0100

    stvrta prednaska

---
+++ 04/src/Cab.java
@@ -0,0 +1,5 @@
+class Cab { // trieda C spája triedy A + B
+	private A a = new A(); // vložená referencia (!) na objekt a typu A
+	private B b = new B(); // vložená referencia (!) na objekt b typu B
+
+}
