9d3eef2f21a1421d8a25ab064d6ab02b2b9625f3
Peter Borovansky <peter.borovansky@microstep-mis.com>
2021-04-09 21:37:12 +0200

    osme cvicenie

--- CV08/src/Basics.java
+++ CV08/src/Basics.java
@@ -1,5 +1,6 @@
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.function.IntPredicate;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -8,112 +9,85 @@ import java.util.stream.Stream;
 
 public class Basics {
 
-    public static void main(String[] args) {
-        //System.out.println(IntStream.range(0,100).boxed().collect(Collectors.toList()));
-        //IntStream.range(0,100).forEach(System.out::print);
-        //IntStream.range(0,100).forEach(n -> System.out.print(n));
-//        IntStream.range(0,100).map(x -> 2*x).forEach(n -> System.out.print(n));
-
-        Predicate<Integer> jeStvorec = n -> Math.pow(Math.floor(Math.sqrt(n)),2) == n;
-        //IntPredicate jeStvorec1 = n -> Math.pow(Math.floor(Math.sqrt(n)),2) == n;
-
-        //IntStream.range(0,100).filter(x -> jeStvorec1.test(x)).forEach(n -> System.out.println(n));
-
-        //IntStream.range(0,100).filter(n -> List.of(2,3,4,5,6,7,8,9).contains(n%10)).forEach(System.out::println);
-
-        //IntStream.range(0,100).filter(n -> IntStream.range(2,10).anyMatch(d -> n%10 == d) )
-        //        .forEach(System.out::println);
-
-        //Predicate<Integer> jePrvocislo = n -> n > 1 && IntStream.range(2, n).allMatch(d -> n%d > 0);
-        //Predicate<Integer> jePrvocislo = n -> n > 1 && IntStream.range(2, 1+(n/2)).allMatch(d -> n%d > 0);
-       // Predicate<Integer> jePrvocislo = n -> n > 1 && IntStream.range(2, 1+(int)Math.floor(Math.sqrt(n))).allMatch(d -> n%d > 0);
-       // IntStream.range(0,100).filter(n -> jePrvocislo.test(n) ).forEach(System.out::println);
-
-        //System.out.println(IntStream.range(0,100+1).sum());
-
-        //System.out.println(IntStream.range(0,100+1).reduce(0, Integer::sum));
-        //System.out.println(IntStream.range(0,100+1).reduce(0, (a,b) -> a+b));
-
-        //System.out.println(IntStream.range(1,10+1).reduce(1, (a,b) -> a*b));
-
-        IntPredicate jePrepona = c -> IntStream.range(0,c).anyMatch(a -> IntStream.range(0,c).anyMatch(b-> a*a+b*b==c*c));
-        //https://www.zborovna.sk/kniznica.php?action=show_version&id=283798
-        IntStream.range(0,100+1).filter(jePrepona::test).forEach(System.out::println);
-
-        // w rovnako 0 a 1
-        Predicate<String> rovnako = str ->
-                str.chars().reduce(0, (a,b) -> (b == (int)'0')?a+1:a-1) == 0;
-
-        System.out.println(rovnako.test("1010"));
-        System.out.println(rovnako.test("1100"));
-        System.out.println(rovnako.test("11001"));
-        System.out.println(rovnako.test("100"));
-
-//        System.out.println(" Delitelne");
-//        delitele23456789(IntStream.range(1, 10000))  // vyrobim si stream cisel 1..9999, poslem ho do funkcie delitelne
-//                .forEach(System.out::println);       // s kazdym prvkom vo vysledom streame spravim System.out.println(prvok)
-//        // prve je okolo 2500, potom okolo 5000
-//
-//        System.out.println("\n Dokonale");
-//        dokonale(IntStream.range(1, 1000))
-//                .forEach(System.out::println);
-//
-//        System.out.println("\n Prepona");
-//        prepona(IntStream.range(1, 30))
-//                .forEach(System.out::println);
-//        // 5, 10, 13, 15, 17, ...
-//
-//        System.out.println("\n Dobre uzatvorkovane");
-//        dobreUzatvorkovane(Stream.of("()()()", "", "((()))()", "())(()", "()(()", "()())"))
-//                .forEach(System.out::println);  // maju sa vypisat iba dobre uzatvorkovane vyrazy, teda prve tri
-//
-//        System.out.println("\n Abeceda stream");
-//        List<String> words = List.of("ahoj", "hello", "aloha", "bye bye", "bon jour", "zdrastvuj", "dobry den", "nazdar", "guten tag", "good morning");
-//        abecedaStream(words).forEach(s -> System.out.println(s.collect(Collectors.toList())));
-//
-//        System.out.println("\n Abeceda mapa");
-//        abecedaMap(words).forEach((k, v) -> System.out.println(k + ": " + v.collect(Collectors.toList())));
-    }
-
-
     public static IntStream delitele23456789(IntStream input) {
-        return null; // TODO
+        //return input.filter(i -> i % 8 == 0 && i % 9 == 0 && i % 6 == 0 && i % 7 == 0 && i % 5 == 0);
+        //return input.filter(n -> IntStream.range(2, 10).anyMatch(d -> n%d == 0));
+        return input.filter(n -> IntStream.range(2, 10).allMatch(d -> n%d == 0));
     }
 
     public static IntStream dokonale(IntStream input) {
-        IntPredicate dokonale = null; // TODO
+//        IntPredicate dokonale = n -> IntStream.range(1,n).filter(i -> n % i == 0).sum() == n;
+//        return input.filter(dokonale);
+
+        IntPredicate dokonale = cislo -> IntStream.range(1,cislo)
+                .filter(delitel -> cislo % delitel == 0)
+                .reduce(0, Integer::sum) == cislo;
         return input.filter(dokonale);
+
     }
 
     public static IntStream prepona(IntStream input) {
-        IntPredicate jeStvorec = null; // TODO
-        // System.out.println("Je 144 stovrec? Malo by byt true, je " + jeStvorec.test(144));
-        // System.out.println("Je 97 stovrec? Malo by byt false, je " + jeStvorec.test(97));
-
-        IntPredicate prepona = null; // TODO
-        // System.out.println("Je 13 prepona? Malo by byt true, je " + prepona.test(13));
-        // System.out.println("Je 9 prepona? Malo by byt false, je " + prepona.test(9));
-
+        IntPredicate jeStvorec = n -> Math.sqrt(n) - Math.floor(Math.sqrt(n)) == 0;
+        IntPredicate prepona = c -> IntStream.range(1,c).anyMatch(a -> jeStvorec.test(c*c-a*a));
         return input.filter(prepona);
     }
 
     public static Stream<String> dobreUzatvorkovane(Stream<String> input) {
-        Predicate<String> jeOK = str -> str.chars().boxed()
-                .reduce(0, null /* TODO */  ) == 0;
+        Predicate<String> jeOK = stri ->
+                stri.chars().boxed().reduce(0, (a, b) ->
+                        (((((char)(int)b == '(')?(a+1):(a-1)) < 0)?Integer.MIN_VALUE:
+                                (((char)(int)b == '(')?a+1:a-1))) == 0;
         return input.filter(jeOK);
     }
 
-    public static Stream<Stream<String>> abecedaStream(List<String> words) {
-        Stream<String> abeceda = null; // TODO
-        // Ak je nasledujuci riadok aktivny, tak pri dalsej praci s 'abeceda' vam to padne.
-        // Ak ste teda dobre vygenerovali abecedu, zakomentujte/zmazte tento vypis.
-        // System.out.println(abeceda.collect(Collectors.toList()));
-
-        return abeceda.map(null /* TODO */);
+    public static Stream<Stream<String>> abecedaStream(List<String> words){
+        Stream<String> abeceda = Stream.of(
+                "a", "b", "c", "d", "e",
+                "f", "g", "h", "i", "j",
+                "k", "l", "m", "n", "o", "p",
+                "q", "r", "s", "t", "u",
+                "v", "w", "x", "y", "z");
+        return abeceda.map(pismeno -> words.stream().filter(x -> x.startsWith(pismeno)));
     }
+    public static Map<String, Stream<String>> abecedaMap(List<String> words){
+        List<String> abeceda = List.of(
+                "a", "b", "c", "d", "e",
+                "f", "g", "h", "i", "j",
+                "k", "l", "m", "n", "o", "p",
+                "q", "r", "s", "t", "u",
+                "v", "w", "x", "y", "z");
+        Map<String, Stream<String>> res = new TreeMap<String, Stream<String>>();
+        for (String key : abeceda)  /// kapitulacia :)
+            res.put(key, words.stream().filter(s -> s.startsWith(key)));
+        return res;
+    }
+    public static void main(String[] args) {
+        for (int m = 0; m <Integer.MAX_VALUE; m++) {
+            IntPredicate jeStvorec1 = n -> Math.sqrt(n) - Math.floor(Math.sqrt(n)) == 0;
+            IntPredicate jeStvorec = n -> Math.abs(Math.floor(Math.sqrt(n)) - Math.sqrt(n)) < 0.000001;
+            if (jeStvorec.test(m) != jeStvorec1.test(m))
+                System.out.println("kontrepriklad " + m);
+        }
+        System.out.println(
+                delitele23456789(IntStream.range(0,10000)).boxed().collect(Collectors.toList())
+        );
+        System.out.println(
+                delitele23456789(
+                        Stream
+                                .iterate(0, i->i+4)
+                                .limit(10000)
+                                .mapToInt(e -> (int)e)
+                ).boxed().collect(Collectors.toList())
+        );
+        System.out.println(
+                dokonale(IntStream.range(0,10000)).boxed().collect(Collectors.toList())
+        );
+        System.out.println(
+                prepona(IntStream.range(0,10000)).boxed().collect(Collectors.toList())
+        );
+        System.out.println(
+                dobreUzatvorkovane(Stream.of("()", ")(", "(()")).collect(Collectors.toList())
+        );
 
-    public static Map<String, Stream<String>> abecedaMap(List<String> words) {
-        Stream<String> abeceda = null; // TODO
-        return abeceda.collect(Collectors.toMap(null, null /* TODO */));
     }
 }
--- CV08/src/FibTree.java
+++ CV08/src/FibTree.java
@@ -1,24 +1,60 @@
-import java.io.FileInputStream;
-import java.io.IOError;
-import java.io.IOException;
-import java.io.ObjectInputStream;
+import java.io.*;
 
 public class FibTree {
 
     public static <E> E load(String fileName) {
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(fileName))) {
-            return (E) ois.readObject();
+            return (E)ois.readObject();
         } catch (IOException | ClassNotFoundException e) {
+            System.err.println("nieco zle sa stalo pri citani suboru " + fileName);
             e.printStackTrace();
-            return  null;
+            return null;
+        }
+    }
+    // mozno sa za zide...
+    public static <E> void save(String fileName, E t) {
+        try (ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(fileName))) {
+            os.writeObject(t);
+        } catch (IOException e) {
+            System.err.println("nieco zle nastalo pri otvarani suboru " + fileName +
+                    ": " + e.getMessage());
+            e.printStackTrace();
         }
     }
 
-    public static void main(String[] args) {
-        Node n = load("du8.bin");
-        System.out.println(n.value);
-        for(int i = 0; i <30; i++)
-            System.out.println(i + " -> " + new Node(i).value);
+    private static long sumr(Node t) {
+        if (t == null) return 0;
+        return t.value +
+                ((t.left!=null)?sumr(t.left):0) +
+                ((t.right!=null)?sumr(t.right):0);
     }
+    public static long sum(String fileName) {
+        return sumr((Node)load(fileName));
+    }
+    public static Node next(String fileName) {
+        Node t = (Node)load(fileName);
+        int level = isFibTree(t);
+        return new Node(level+1);
+    }
+    public static int isFibTree(Node t) {
+        if (t == null) return -1;
+        if (t.value == 0 && t.left == null && t.right == null)
+            return 0;
+        if (t.value == 1 && t.left == null && t.right == null)
+            return 1;
+        if (t.left != null && t.right != null) {
+            int l = isFibTree(t.left);
+            int r = isFibTree(t.right);
+            return (l >= 0 && r >= 0 && l+1 == r)?r+1:-1;
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        Node t = (Node)load("du8.bin");
+        System.out.println(t.value);
+        for(int i = 0; i< 30; i++) {
+            System.out.println(i + " -> " + new Node(i).value);
+        }
 
+    }
 }
