dcaf0b9bc8533f81dcfce2c211978d02a882984a
Peter Borovansky <Peter.Borovansky@microstep-mis.com>
2026-03-06 08:38:41 +0100

    treti tyzden

--- 03/src/Complx.java
+++ 03/src/Complx.java
@@ -4,6 +4,11 @@ public class Complx {
     private double real;    // realna cast
     private double imag;    // imaginarna cast
 
+    public Complx(double real, double imag) {
+        this.real = real;
+        this.imag = imag;
+    }
+
     public double getReal() {
         return real;
     }
@@ -28,32 +33,14 @@ public class Complx {
         this.xxx = xxx;
     }
 
-    private boolean xxx;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Complx complx = (Complx) o;
-        return Double.compare(complx.real, real) == 0 && Double.compare(complx.imag, imag) == 0 && xxx == complx.xxx;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(real, imag, xxx);
-    }
-
     @Override
     public String toString() {
-        final StringBuffer sb = new StringBuffer("Complx{");
-        sb.append("real=").append(real);
-        sb.append(", imag=").append(imag);
-        sb.append('}');
-        return sb.toString();
+        return "Complx{" +
+                "real=" + real +
+                ", imag=" + imag +
+               //, xxx=" + xxx +
+                '}';
     }
 
-    public Complx(double real, double imag) {
-        this.real = real;
-        this.imag = imag;
-    }
+    private boolean xxx;
 }
